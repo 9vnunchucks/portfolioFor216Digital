c9fe1aae82bc8631bc4b487f23b9f1747e037884
Ryan Foltz <ryan.l.foltz@gmail.com>
2018-06-15 01:01:36 -0400

    parallax template updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,20 @@
-
-$.fn.isInViewport = function() {
-var elementTop = $(this).offset().top;
-var elementBottom = elementTop + $(this).outerHeight();
-
-var viewportTop = $(window).scrollTop();
-var viewportBottom = viewportTop + $(window).height();
-
-return elementBottom > viewportTop && elementTop < viewportBottom;
-};
-
-
-$(window).on('resize scroll', function() {
-    if ($('.onScroll').isInViewport()) {
-      $("onScroll").attr(
-        
-      )
-    // } else {
-    //   $('.graphic').hide();
-    }
-  });
+//
+// $.fn.isInViewport = function() {
+// var elementTop = $(this).offset().top;
+// var elementBottom = elementTop + $(this).outerHeight();
+//
+// var viewportTop = $(window).scrollTop();
+// var viewportBottom = viewportTop + $(window).height();
+//
+// return elementBottom > viewportTop && elementTop < viewportBottom;
+// };
+//
+//
+// $(window).on('resize scroll', function() {
+//     if ($('.onScroll').isInViewport()) {
+//
+//
+//      } else {
+//
+//     }
+//   });
